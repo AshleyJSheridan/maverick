89f81ecce675f76028f5bb199ddb612cc0462e4a
Ashley Sheridan <ash@ashleysheridan.co.uk>
2015-01-18 22:48:03 +0000

    tested db abstration layer

--- maverick/models/content.php
+++ maverick/models/content.php
@@ -11,10 +11,10 @@ class content
 		$data = db::table('test AS t')
 			->leftJoin('test2 AS t2', array(
 				array('t2.test_id', '=', 't.id'),
-				array('t2.display', '=', db::raw('yes')),
+				//array('t2.display', '=', db::raw('yes')),
 			))
 			//->leftJoin('test2 AS t2', array('t2.test_id', '=', 't.id') )
-			->where('t.field_key', '=', db::raw('test'))
+			//->where('t.field_key', '=', db::raw('test'))
 			//->where(db::raw(1), '=', db::raw(1))
 			//->groupBy('t2.other_value', 'desc')
 			//->get(array('t.id', 't.field_key', 't.field_value', 't2.id AS test2_id', 't2.other_value'))
