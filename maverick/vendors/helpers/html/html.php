b1a7da1282d20c567a43008287680d1e343eafda
Ashley Sheridan <ash@ashleysheridan.co.uk>
2015-04-04 10:51:10 +0100

    html snippet loader and parser now caches snippets it loads to avoid reading in the same files repeatedly - also removed some old form html from the form view as it's now using the form html helper class

--- maverick/vendors/helpers/html/html.php
+++ maverick/vendors/helpers/html/html.php
@@ -3,26 +3,44 @@ namespace helpers\html;
 
 class html
 {
+	static $_instance;
+	private $cached_snippets = array();
 	
+	private function __construct() {}
 	
-	public function __construct()
+	private function __clone() {}
+	
+	public static function getInstance()
 	{
-		
+		if(!(self::$_instance instanceof self))
+			self::$_instance = new self;
+
+		return self::$_instance;
 	}
 	
 	public static function load_snippet($filename, $replacements)
 	{
-		//TODO: cache these views so that they don't require reading in the same files over and over
+		$h = html::getInstance();
+
 		if(!file_exists($filename))
 			return false;
 		
 		$contents = '';
 		$find = $replace = array();
 
-		$fh = fopen($filename, 'r');
-		while (!feof($fh))
-			$contents .= fread($fh, 8192);
-		fclose($fh);
+		if(isset($h->cached_snippets[$filename]))
+			$contents = $h->cached_snippets[$filename];
+		else
+		{
+			$fh = fopen($filename, 'r');
+			while (!feof($fh))
+				$contents .= fread($fh, 8192);
+			fclose($fh);
+			
+			var_dump($filename);
+		}
+		
+		$h->cached_snippets[$filename] = $contents;
 		
 		// replace the sections in the template snippet with the values from the array passed in
 		foreach($replacements as $key => $value)
