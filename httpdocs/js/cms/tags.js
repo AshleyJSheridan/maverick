b3f3c308ccd6ccf671416a10b0a49301aa16216c
Ashley Sheridan <ash@ashleysheridan.co.uk>
2015-07-22 18:38:12 +0100

    added js stubb for tags

---
+++ httpdocs/js/cms/tags.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+;(function(MAV, $){
+	$(function() {
+		MAV.Tags.init();
+	});
+	
+	MAV.Tags = {
+		init : function(){
+			
+			
+		}
+	};
+})(window.MAV = window.MAV || {}, jQuery);
